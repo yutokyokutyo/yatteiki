a5d8618595bfebc4269d9ca1c9a5703b9244ac4e
y.kondo <y.kondo@pepabo.com>
2017-04-05 12:41:35 +0900

    アジャイルプラクティスを追加

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ words = [
     "今を全力 -> 明日が楽しい",
     "思考は明文化しろ",
     "肩甲骨を突き上げろ",
+    "人に魚を与えればそれは一日の糧となる。しかし魚の釣り方を教えれば、それは一生の糧となる",
 ]
 var word = words[Math.floor(Math.random() * words.length)];
 document.getElementById('word')
